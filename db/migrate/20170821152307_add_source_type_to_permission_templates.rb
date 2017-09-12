d2e386e88b448a65e3836f1ed1be4ebd67f17070
Blanco <blancoj@umich.edu>
2017-09-12 16:36:12 -0400

    during migration set source_type in permission_template to 'admin_set'

--- db/migrate/20170821152307_add_source_type_to_permission_templates.rb
+++ db/migrate/20170821152307_add_source_type_to_permission_templates.rb
@@ -1,7 +1,13 @@
 class AddSourceTypeToPermissionTemplates < ActiveRecord::Migration[4.2]
   def change
     add_column :permission_templates, :source_type, :string
-    rename_column :permission_templates, :admin_set_id, :source_id
+
+    remove_column :permission_templates, :admin_set_id
+    add_column :permission_templates, :source_id, :string
+
+    Hyrax::PermissionTemplate.find_each do |permission_template|
+      permission_template.source_type = 'admin_set'
+      permission_template.save!
+    end
   end
 end
-
