fd0491cf8dc64eac9e3178377f622d5b4b439454
Karen Didrickson <karendid@gmail.com>
2017-09-13 14:59:15 -0500

    CollectionTypeParticipant model validations

--- spec/models/hyrax/collection_type_participant_spec.rb
+++ spec/models/hyrax/collection_type_participant_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe Hyrax::CollectionTypeParticipant, type: :model do
     expect(collection_type_participant).to respond_to(:access)
     expect(collection_type_participant.access).not_to be_empty
   end
+
+  describe "validations", :clean_repo do
+    describe '#agent_id' do
+      it { is_expected.to validate_presence_of(:agent_id) }
+    end
+    describe '#agent_type' do
+      it { is_expected.to validate_presence_of(:agent_type) }
+      it { is_expected.not_to allow_value('somethingelse').for(:agent_type) }
+      it { is_expected.to allow_value(Hyrax::CollectionTypeParticipant::USER_TYPE).for(:agent_type) }
+      it { is_expected.to allow_value(Hyrax::CollectionTypeParticipant::GROUP_TYPE).for(:agent_type) }
+    end
+    describe '#access' do
+      it { is_expected.to validate_presence_of(:access) }
+      it { is_expected.not_to allow_value('somethingelse').for(:access) }
+      it { is_expected.to allow_value(Hyrax::CollectionTypeParticipant::MANAGE_ACCESS).for(:access) }
+      it { is_expected.to allow_value(Hyrax::CollectionTypeParticipant::CREATE_ACCESS).for(:access) }
+    end
+    describe '#hyrax_collection_type_id' do
+      it { is_expected.to validate_presence_of(:hyrax_collection_type_id) }
+    end
+  end
 end
